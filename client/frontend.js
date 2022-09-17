66bd209a7fe79409c9746e73863cf9bee80d0e4c
fakincow <zabaroth@gmail.com>
2022-09-18 02:04:31 +0300

    done with front template

--- client/frontend.js
+++ client/frontend.js
@@ -9,15 +9,29 @@ new Vue(
                     name: '',
                     value: ''
                 },
-                contacts: []
+                contacts: [
+                    {id:1, name: 'Roman', value: '0548390923', marked: false}
+                ]
             }
+        },
+        computed: {
+          canCreate(){
+              return this.form.value.trim() && this.form.name.trim();
+          }
         },
         methods: {
             createContact(){
                 const {...contact } = this.form;
-                this.contacts.push({...contact, id: Date.now() })
+                this.contacts.push({...contact, id: Date.now(), marked: false })
                 this.form.name = this.form.value = ''
-            }
+            },
+            markContact(id){
+              const contact = this.contacts.find(c => c.id === id);
+              contact.marked = true;
+            },
+            removeContact(id){
+                this.contacts = this.contacts.filter((con) => con.id !== id)
+            },
         }
     }
 )
